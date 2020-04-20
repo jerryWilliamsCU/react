0b29a86e2d0d08185b147f6361b264dbcf806836
jerryWilliamsCU <historian1974@gmail.com>
2020-04-20 15:47:38 -0400

    Configuring React

---
+++ App.js
@@ -0,0 +1,32 @@
+
+import { Navbar, NavbarBrand } from 'reactstrap';
+import Directory from './components/DirectoryComponent';
+import React, { Component } from 'react';
+import { CAMPSITES } from './shared/campsites';
+import './App.css';
+
+class App extends Component {
+  constructor(props) {
+      super(props);
+      this.state = {
+          campsites: CAMPSITES
+      };
+  }
+
+
+
+  render() {
+      return (
+          <div className="App">
+              <Navbar dark color="primary">
+              <div className="container">
+              <NavbarBrand href="/">NuCamp</NavbarBrand>
+              <Directory campsites={this.state.campsites} />
+                </div>
+              </Navbar>
+          </div>
+      );
+  }
+}
+
+export default App;
